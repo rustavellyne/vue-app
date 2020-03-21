ccdc716cbd0bd04def27e3f31c89b0148f0542a0
Alex Chernyakov <rustavellyne@gmail.com>
2020-03-22 00:48:48 +0200

    add login form

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,53 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Home from '@/components/Home'
+import Ad from '@/components/Ads/Ad'
+import AdList from '@/components/Ads/AdList'
+import NewAd from '@/components/Ads/NewAd'
+import Orders from '@/components/User/Orders'
+import Login from '@/components/Auth/Login'
+import Register from '@/components/Auth/Register'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
-    routes: [],
+    routes: [
+        {
+            path: '',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/ad/:id',
+            name: 'ad',
+            component: Ad,
+        },
+        {
+            path: '/list',
+            name: 'adlist',
+            component: AdList,
+        },
+        {
+            path: '/new',
+            name: 'newad',
+            component: NewAd,
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login,
+        },
+        {
+            path: '/registration',
+            name: 'reg',
+            component: Register,
+        },
+        {
+            path: '/orders',
+            name: 'orders',
+            component: Orders,
+        },
+    ],
+    // withou hash
+    mode: 'history',
 });
