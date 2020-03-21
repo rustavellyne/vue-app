b1f327a94a4eb3b1b1505b6a650862061e72811d
Alex Chernyakov <rustavellyne@gmail.com>
2020-03-22 00:05:53 +0200

    add sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,60 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-navigation-drawer temporary app v-model="drawer">
+        <v-list >
+          <v-list-item :to="link.url" v-for="link of links" :key="link.title">
+            <v-list-item-icon>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-icon>
+  
+            <v-list-item-content>
+              <v-list-item-title v-text="link.title"></v-list-item-title>
+            </v-list-item-content>
+  
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-card
+        color="grey lighten-4"
+        flat
+        tile
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
+        <v-app-bar dark app flat color="primary">
+          <v-app-bar-nav-icon 
+            @click="drawer = !drawer" 
+            class="hidden-md-and-up"
+          ></v-app-bar-nav-icon>
+          <v-toolbar-title>Ad application</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-toolbar-items class="hidden-sm-and-down">
+            
+            <v-btn text small v-for="link of links" :key="link.title" :to="link.url">
+              <v-icon left dark>{{ link.icon }}</v-icon>
+              {{ link.title }}
+            </v-btn>
+          </v-toolbar-items>
+        </v-app-bar>
+      </v-card>
 
     <v-content>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
+  data () {
+    return {
+      drawer: false,
+      links: [
+        { title: 'Login', icon: 'mdi-lock', url: '/login' },
+        { title: 'Registration', icon: 'mdi-face', url: '/registration' },
+        { title: 'Orders', icon: 'mdi-bookmark', url: '/orders' },
+        { title: 'New Ad', icon: 'mdi-plus', url: '/new' },
+        { title: 'My Ad', icon: 'mdi-format-list-bulleted', url: '/list' },
+      ],
+    }
   },
-
-  data: () => ({
-    //
-  }),
-};
+} 
 </script>
