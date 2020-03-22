0fcbffae8863e087fd93d3ae29d621d9195fd1ca
Alex Chernyakov <rustavellyne@gmail.com>
2020-03-22 19:31:31 +0200

    add all components

--- src/components/Ads/Ad.vue
+++ src/components/Ads/Ad.vue
@@ -2,7 +2,22 @@
   <v-container>
     <v-layout row>
       <v-flex xs12>
-        <h1>Ad</h1>
+        <v-card>
+          <v-img
+            class="white--text align-end"
+            height="300px"
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+          ></v-img>
+          <v-card-text>
+            <h1 class="text--primary">Lorem</h1>
+            <p class="text--primary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ab nobis aliquid nam minus, consectetur pariatur eaque quibusdam sequi magni eius accusamus nemo laudantium quasi? Fuga voluptates ab quae fugit.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn depressed color="warning">Edit</v-btn>
+            <v-btn color="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,7 +25,7 @@
 
 <script>
 export default {
-  date () {
+  data () {
     return {
 
     }
--- src/components/Ads/AdList.vue
+++ src/components/Ads/AdList.vue
@@ -1,8 +1,40 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Ad list</h1>
+      <v-flex xs12  sm6 offset-sm-3>
+        <h1 class="text--secondary mb-3">My Ads</h1>
+        <v-card class="mb-4 elevation-3" 
+            v-for="ad in ads" 
+            :key="ad.id"
+        >
+          <v-layout row>
+            <v-flex xs4>
+              <v-img
+              class="white--text align-end"
+              height="150px"
+              :src="ad.img"
+              ></v-img>
+            </v-flex>
+            <v-flex xs8>
+              <v-card-text>
+                <h2 class="text--primary">
+                  {{ ad.title }}
+                </h2>
+                <p>
+                  {{ ad.description }}
+                </p>
+              </v-card-text>
+              
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary"
+                  :to="'ad/' + ad.id"
+                >Open
+                </v-btn>
+              </v-card-actions>
+            </v-flex>
+          </v-layout>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -10,9 +42,38 @@
 
 <script>
 export default {
-  date () {
+  data () {
     return {
-
+      ads: [
+        { 
+          title: 'First Ad', 
+          description: 'some new add', 
+          promo: true, 
+          img: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg', 
+          id: '123'
+        },
+        { 
+          title: 'Second Ad', 
+          description: 'some new add', 
+          promo: true, 
+          img: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg', 
+          id: '124'
+        },
+        { 
+          title: 'Third Ad', 
+          description: 'some new add', 
+          promo: true, 
+          img: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg', 
+          id: '125'
+        },
+        { 
+          title: 'Four Ad', 
+          description: 'some new add', 
+          promo: true, 
+          img: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg', 
+          id: '126'
+        }
+      ],
     }
   },
 }
